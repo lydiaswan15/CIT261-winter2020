2ed3ca17d30815cc1bba7fe59fe5721859887e32
lydiaswan15 <lydiaswan15@gmail.com>
2020-02-15 13:01:31 -0700

    changes

--- Practice/toDoProject2/Todo.js
+++ Practice/toDoProject2/Todo.js
@@ -1,41 +1,7 @@
+import {qs, qsa, storeInLocal, getFromLocal}from "./utilities.js"
 
-let input = document.getElementById('text');
 let totalList = [];
-let totalContent = document.createElement('li');
 
-function format(){
-    document.querySelector('#toDoDiv').innerHTML = "";
-
-    for(let i = 0; i < totalList.length; i++){
-
-        console.log(i);
-
-        //text
-
-        let newContent = `
-        <div>
-        ${totalList[i].content}
-        <button id = ${totalList[i].id}id>Remove</button>
-        <button>Complete</button>
-        </div>`;
-
-        console.log(newContent);
-
-        document.querySelector('#toDoDiv').innerHTML += newContent;
-
-        addEventListener(totalList[i].id+"id");
-        
-    }
-}
-
-function addEventListener(current){
-    
-    document.getElementById(current).addEventListener('click', remove);
-}
-
-function remove(current){
-    console.log("hello");
-}
 
 export default class Todo{
 
@@ -45,20 +11,102 @@ export default class Todo{
     createNewTodo(){
         //adds the text from there to the array of total items.
 
-        let newTodoItem = document.querySelector('#text').value;
-        const newTodo = {id: new Date(), content: newTodoItem, completed: false};
+        let newTodoItem = qs('#text').value;
+        const newTodo = {id: new Date().getTime(), content: newTodoItem, completed: false};
 
         totalList.push(newTodo);
 
+        storeInLocal(totalList);
+
         format();
+
         return this.totalList;
-    }
 
-    remove(){
-        console.log("worked");
     }
     
-    
-
-
 }
+
+
+function format(){
+    qs('#toDoDiv').innerHTML = "";
+
+    totalList = getFromLocal();
+
+    for(let i = 0; i < totalList.length; i++){
+        //text
+
+        let newContent = `
+        <div class = 'item'>
+        ${totalList[i].content}
+        <div class = 'buttons'>
+        <button class = 'remove' id = ${totalList[i].id} >Remove</button>
+        <button class = 'complete' id = ${totalList[i].id}>Complete</button>
+        </div>
+        </div>`;
+
+       
+        //adding the new content to the div on the HTML
+
+
+        if (totalList[i].completed == true){
+            newContent = `
+                <div class = 'item completedItem'>
+                    ${totalList[i].content}
+                    <div class = 'buttons'>
+                        <button class = 'remove' id = ${totalList[i].id} >Remove</button>
+                        <button class = 'complete' id = ${totalList[i].id}>Complete</button>
+                    </div>
+                </div>`;
+
+        }
+
+        qs('#toDoDiv').innerHTML += newContent;
+        
+    }
+    eventListeners(totalList);
+ 
+}
+
+function eventListeners(totalList){
+    qsa('.remove').forEach(item =>{
+        item.addEventListener('click', remove);
+    });
+    
+    qsa('.complete').forEach(item =>{
+        item.addEventListener('click', complete);
+    });
+}
+
+
+function remove(e){
+   
+    let index = -1;
+    totalList.forEach((item, i) =>{
+        if(item.id == e.target.id){
+            index = i;
+        }
+    })
+
+    totalList.splice(index, 1);
+
+    storeInLocal(totalList);
+    
+    format();
+   //find the index of 
+}
+
+function complete(e){
+    let index = -1;
+    totalList.forEach((item, i) =>{
+        if(item.id == e.target.id){
+            index = i;
+        }
+    })
+
+    totalList[index].completed = true;
+    
+    storeInLocal(totalList);
+
+    format(); 
+}
+
